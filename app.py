6cb3969a88eaebc2b8dd53a22e43ad0854396e91
Irakli Khuchua <irakli.khuchua.1@iliauni.edu.ge>
2022-02-01 14:27:02 +0400

    davaleba 9

--- app.py
+++ app.py
@@ -1,54 +1,21 @@
-class Shop:
-    name = None
-    SelectedItems = None
-    qty = None
-    products = None
-    choice = None
+from flask import Flask
+from flask_restful import Resource, Api
+import sqlite3
 
-    def __init__(self, prod):
-        self.products = prod
+con = sqlite3.connect("identifier.sqlite", check_same_thread=False)
+cur = con.cursor()
 
-    def sayHello(self):
-        while not bool(self.name):
-            self.name = input("Hello, what is your name? \n")
-
-    def showMenu(self):
-        print(f"Hello {self.name}! Please have a look at our Menu:")
-        for i in product_list:
-            print(i)
-
-    def getChoice(self):
-        while self.choice not in self.products:
-            self.choice = input("Now please choose your product: \n").upper()
-
-    def getqty(self):
-        self.qty = input("Choose quantity: \n")
-        while self.qty.isdigit() != True:
-            self.qty = input("Choose only numbers! \n")
-
-    def calculate(self):
-        return self.products.get(self.choice) * float(self.qty)
-
-    def showCalculate(self):
-        print("Total sum of your product is {} USD.".format(self.calculate()))
-
-    def Goodbye(self):
-        print(f"Thank you {self.name} for visiting us. \nBy by!")
+app = Flask(__name__)
+api = Api(app)
 
 
-product_list = {
+class Students(Resource):
+    def get(self, student_id):
+        cur.execute("SELECT * FROM test where id = ?", (student_id,))
+        return cur.fetchone()
 
-    "A": 2,
-    "B": 3,
-    "C": 4,
-    "D": 5,
-    "E": 6,
-}
 
-shop = Shop(product_list)
-shop.sayHello()
-shop.showMenu()
-shop.getChoice()
-shop.getqty()
-shop.showCalculate()
-shop.Goodbye()
+api.add_resource(Students, '/<int:student_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
